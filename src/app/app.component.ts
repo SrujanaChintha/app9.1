0df2610cdc6ff12989f058cce87a118f9b9e9194
Chintha Srujana <157452984+SrujanaChintha@users.noreply.github.com>
2024-11-23 10:36:47 +0530

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LifecycleDemoComponent } from './lifecycle-demo/lifecycle-demo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,LifecycleDemoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'app9.1';
+  title = 'app9';
 }
